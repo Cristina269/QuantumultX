f0c37a7714a30349388398f80711eb00fccecb7c
Curtin <curtinp118@gmail.com>
2026-05-28 15:45:56 +0800

    refactor(caiyun): var→const, 重命名变量 aFengYe→body

--- scripts/caiyun.js
+++ scripts/caiyun.js
@@ -1,21 +1,20 @@
 /******************************
 脚本功能：彩云天气-登陆后解锁会员
 更新时间：2024-06-10
+作者：Curtinp118
 *******************************
 [rewrite_local]
 ^https:\/\/biz\.cyapi\.cn\/ url script-response-body https://raw.githubusercontent.com/curtinp118/QuantumultX/refs/heads/main/scripts/caiyun.js
-[mitm] 
+[mitm]
 hostname = biz.cyapi.cn
 *******************************/
 
-var aFengYe = $response.body;
-var obj =  JSON.parse(aFengYe);
+const body = JSON.parse($response.body);
 
-if($request.url.indexOf("/v2/user") != -1) {
-  obj.result.is_vip = true;
-  obj.result.vip_type = "s";
-  obj.result.svip_expired_at = 4070951226;
+if ($request.url.indexOf("/v2/user") !== -1) {
+  body.result.is_vip = true;
+  body.result.vip_type = "s";
+  body.result.svip_expired_at = 4070951226;
 }
 
-aFengYe = JSON.stringify(obj);
-$done(aFengYe);
+$done(JSON.stringify(body));
